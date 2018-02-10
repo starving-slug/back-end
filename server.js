90c3cdfd6687bc41e4f050e3d437c6de4b84665e
Shashank Guduru <shashankguduru@eduroam-169-233-233-225.ucsc.edu>
2018-02-10 13:32:39 -0800

    reorganized project hiararchy

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require('./config/config');
+require('config/config');
 
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {mongoose} = require('db/mongoose');
+var {Todo} = require('models/todo');
+var {User} = require('models/user');
 
 var app = express();
 const port = process.env.PORT;
