c6b4a6e5ed285bc2359efc69e33fd095b5b14720
Danny Boyd <dacboyd@ucsc.edu>
2018-02-26 11:50:14 -0800

    Simple profile support

---
+++ server/models/profile.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+var Profile = mongoose.model('Profile', {
+  username: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+    minlength: 1
+  },
+  image: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  description: {
+    type: String,
+    require: false,
+    minlength: 1,
+  }
+});
+
+module.exports = {Profile}
--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const Promise = require('bluebird');
 
 let {mongoose} = require('./db/mongoose');
 let {User} = require('./models/user');
 let {Recipe} = require('./models/recipe');
+let {Profile} = require('./models/profile');
 
 let app = express();
 const port = process.env.PORT;
@@ -32,7 +34,7 @@ app.post('/users', (req, res) => {
   user.save().then((user) => {
     res.send(user);
   }).catch((e) => {
-    res.status(400).send(e);
+    res.status(400).send({message: e.message});
   })
 });
 
@@ -46,15 +48,70 @@ app.get('/users/:username',(req, res) => {
     }
     res.status(404).send();
   }).catch((e) => {
-    res.status(400).send(e);
+    res.status(400).send({message: e.message});
   });
 });
+
+// POST profile
+app.post('/setProfile', (req, res) => {
+  let body = _.pick(req.body, ['user', 'description', 'image']);
+  console.log(body);
+  // test user for valid session
+  let profile = new Profile({username: body.user.name, image: body.image, description: body.description});
+
+  profile.save().then((foo) => {
+    console.log(foo);
+    res.status(200).send({message: `Successfully saved profile for ${body.user.name}`});
+  }).catch((e) => {
+    console.log(e.message);
+    res.status(400).send({message: e.message});
+  });
+})
+
+// Get user profile
+app.get('/profile/:username', (req, res) => {
+  let uname = req.params.username;
+  let recipeList = Recipe.find({'author': uname}, '_id name description');
+  let profileReq = Profile.findOne({'username': uname}, 'username image description');
+
+  // let promise = Promise.join(recipeList, profileReq, function(recipes, profile) {
+  //   console.log(recipes, profile);
+  //   if (recipes && profile) {
+  //     let response = {
+  //       username: profile.username,
+  //       description: profile.description,
+  //       image: profile.image,
+  //       recipes: recipes
+  //     }
+  //     res.status(200).send(response);
+  //   }
+  //   res.status(404).send();
+  // }).catch((e) => {
+  //   res.status(400).send({message: e.message});
+  // })
+
+  Recipe.find({'author': uname}, '_id name description').then((recipes) => {
+    console.log(recipes);
+    if (recipes) {
+      let response = {
+        usename: uname,
+        description: 'Test description',
+        recipes: recipes
+      }
+      console.log(response)
+      res.status(200).send(response);
+    }
+    res.status(404).send();
+  }).catch((e) => {
+    res.status(400).send({message: e.message})
+  })
+})
 
 // POST /recipe
 app.post('/recipe', (req, res) => {
   let body = _.pick(req.body, ['name', 'author', 'description', 'photo', 'ingredients', 'directions', 'tags']);
   let recipe = new Recipe(body);
-  
+
 
   recipe.save().then((recipe) => {
     res.status(200).send('Successfully saved recipe!');
@@ -67,18 +124,25 @@ app.post('/recipe', (req, res) => {
 // GET /recipe/:id
 app.get('/recipe/:id', (req, res) => {
   let id = req.params.id;
+  console.log('1');
 
   // if id is not valid
   if(!ObjectID.isValid(id)) {
-    res.send(400).send(); // bad request
+    console.log('2')
+    res.status(400).send({message: 'Recipe ID is invalid'}); // bad request
+    return;
   }
 
   Recipe.findById(id, (err, recipe) => {
+    console.log('3');
     if(recipe) {
+      console.log('success!');
       res.status(200).send(recipe);
     }
+    console.log('not found');
     res.status(404).send();
   }).catch((e) => {
+    console.log(e);
     res.status(400).send(e);
   });
 
