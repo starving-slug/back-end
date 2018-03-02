a8bd919da1e78968ddaba91cb52feb83e58cb910
karengl <kglee@ucsc.edu>
2018-03-01 22:23:47 -0800

    updated GET search. Also added a PATCH recipe to update current recipes

--- server/server.js
+++ server/server.js
@@ -58,19 +58,17 @@ app.get('/users/:username',(req, res) => {
 // GET /search
 app.get('/search', (req, res) => {
   // Example test:
-  // GET localhost:3000/search?author=theShaGu&tag=omega-3&tag=Meat
-  // req.query.author = theShaGu
-  // req.query.tag = ['omega-3', 'Meat']
-  console.log(req.query.name);
-  console.log(req.query.author);
-  console.log(req.query.tag);
+  // GET localhost:3000/search?author=KevLoi&tag=omega&tag=Meat
+  // req.query.author = KevLoi
+  // req.query.tag = ['omega', 'Meat']
+  // returns all recipes by author KevLoi that has both tag substrings, omega and Meat
+  // or simple a test: GET localhost:3000/search => returns all recipes
+  let nameList = setRegex(req.query.name);
+  let authorList = setRegex(req.query.author);
+  let tagList = setRegex(req.query.tag);
 
-  let nameList = req.query.name; // Search only works for one name
-  let authorList = req.query.author;
-  let tagList = req.query.tag;
-
-  // Search by recipe fields
-  Recipe.find({$or: [{'name': {'$regex': nameList, '$options': 'i'}}, {'author': authorList}, {'tags.text': tagList}]}, '_id name author description ingredients tags').then((recipes) => {
+  // Search: { (n1 OR n2 OR ... OR nk) AND (a1 OR a2 OR ... OR ak) AND (tag1 AND tag2 AND ... AND tagk) }
+  Recipe.find({'$and': [{'name': nameList}, {'author': authorList}, {'tags.text': {'$all': tagList}}]}, '_id name author description tags').then((recipes) => {
     console.log(recipes);
     if (recipes) {
       let response = recipes;
@@ -82,6 +80,21 @@ app.get('/search', (req, res) => {
     res.status(400).send({message: e.message})
   });
 });
+
+// Transform tag(s) in a string (or array) into a regex that ignores capitalization
+function setRegex(list) {
+  let listReg = [];
+  if (typeof list == "string") { // One tag (string)
+    listReg = new RegExp(list, "i");
+  }else if (Object.prototype.toString.call(list) == "[object Array]") { // Multiple tags (array)
+    //listReg = new RegExp(list.join("|"), "i"); // OR (word1|word2)
+    listReg = list.map(function(word) { return new RegExp(word, "i"); });
+  }else { // No tags (undefined)
+    listReg = new RegExp(".*", "i");
+  }
+  console.log(listReg);
+  return listReg;
+}
 
 // POST profile
 app.post('/setProfile', (req, res) => {
@@ -100,7 +113,7 @@ app.post('/setProfile', (req, res) => {
 })
 
 // Get user profile
-app.get('/', (req, res) => {
+app.get('/profile/:username', (req, res) => {
   let uname = req.params.username;
   let recipeList = Recipe.find({'author': uname}, '_id name description');
   let profileReq = Profile.findOne({'username': uname}, 'username image description');
@@ -190,6 +203,29 @@ app.get('/recipe/:id', (req, res) => {
   //   res.status(400).send(e);
   // });
 })
+
+// PATCH /recipe/edit/:id
+app.patch('/recipe/edit/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['name', 'author', 'description', 'photo', 'ingredients', 'directions', 'tags']);
+
+  console.log(body);
+  console.log("finding recipe");
+  if(!ObjectID.isValid(id)) {
+    res.status(400).send({message: 'Recipe ID is invalid'}); // bad request
+    return;
+  }
+
+  // Needs checking, probably pretty shady
+  Recipe.update({"_id": ObjectID(id)}, {"$set" : body}, function(err, recipe) {
+    if (err) {
+      console.log("Error updating recipe: " + err);
+      res.send({"error": "An error has occured"});
+    }else {
+      console.log("recipe updated");
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
