26bfe4f968d78a8fce1eaeda5f849827049e70de
KevLoi <w.kevnloi@gmail.com>
2018-03-12 11:40:47 -0700

    Tested POST /search by name, recipe, and author

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// Starvingslugs database
-mongoose.connect('mongodb://admin:NGyqgMFVRtGXbejhsB6coggX@ds111568.mlab.com:11568/starvingslugs', { useMongoClient: true } );
+// // Starvingslugs database
+// mongoose.connect('mongodb://admin:NGyqgMFVRtGXbejhsB6coggX@ds111568.mlab.com:11568/starvingslugs', { useMongoClient: true } );
 
-// // test database
-// mongoose.connect('mongodb://admin:helloWorld123@ds012578.mlab.com:12578/back-end-test', { useMongoClient: true } );
+// test database
+mongoose.connect('mongodb://admin:helloWorld123@ds012578.mlab.com:12578/back-end-test', { useMongoClient: true } );
 
 module.exports = {mongoose};
